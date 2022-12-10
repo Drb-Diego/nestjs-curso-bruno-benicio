619f0bb879adbaa7491f70d726920afa57caaf0b
Diego Brito <diegorbrito9@gmail.com>
2022-12-10 12:20:34 -0300

    feat: adicionando o 'apiTags' do user na documentação

--- src/entities/User/user.controller.ts
+++ src/entities/User/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
+@ApiTags('Usuários')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
