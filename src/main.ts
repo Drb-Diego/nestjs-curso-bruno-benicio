609600d4cbb006896864cd1701493ab6d94a6542
Diego Brito <diegorbrito9@gmail.com>
2022-12-10 11:49:26 -0300

    feat: criando o setup do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
   const { PORT } = process.env;
-
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Api do curso de nestjs')
+    .setDescription('Uma api de estudo do nestjs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
