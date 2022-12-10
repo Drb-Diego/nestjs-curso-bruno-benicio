e0022a27eca6fa3ce4656d7191e7f704f91b42e1
Diego Brito <diegorbrito9@gmail.com>
2022-12-10 11:00:00 -0300

    new: instalando dotenv e adicionando a variavel de ambiente no main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
+  const { PORT } = process.env;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
